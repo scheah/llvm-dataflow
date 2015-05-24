83c9cb63356f875ed2a544037d2ad0bbfa9a4963
scheah <sebcheah@gmail.com>
2015-05-24 12:04:01 -0700

    Get operands. Get destination attempt.

--- llvm/src/lib/CSE231/blockorder.cpp
+++ llvm/src/lib/CSE231/blockorder.cpp
@@ -13,6 +13,7 @@
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/User.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Module.h"
@@ -33,8 +34,29 @@ namespace {
 			errs() << "Begin block with name: ";
 			errs().write_escaped(B.getName()) << "\n";
 			for (BasicBlock::iterator I = B.begin(); I != B.end(); ++I) { 
-				if (I->isBinaryOp() || StoreInst::classof(I)) {
+				if (I->isBinaryOp() || StoreInst::classof(I) || I->isShift()) { //filter out instructions that we can compute dataflow facts from
 					I->dump(); //this prints to console immediately
+					
+					
+					if(StoreInst::classof(I) ) {
+						StoreInst * storeInst = (StoreInst*) I; //this cast is fucking broken
+						errs() << "\toperand: \n";
+						errs() << "\t\t";
+						storeInst->getValueOperand()->dump();
+						
+						errs() << "\tdest: \n";
+						storeInst->getPointerOperand()->dump();
+					}
+					else {
+						errs() << "\toperands: \n";
+						for (unsigned int i = 0; i < I->getNumOperands(); i++) {
+							Value * operand = I->getOperand(i);
+							errs() << "\t\t";
+							operand->dump();
+						}
+					}
+					
+
 				}
 			}
 			return false;
