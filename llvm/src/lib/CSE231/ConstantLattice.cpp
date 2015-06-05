95c014592592f017dce6d9c79da10b689b66933d
Patrick Bird <patbird@gmail.com>
2015-06-05 01:11:13 -0700

    Change how lattice is dumped.

--- llvm/src/lib/CSE231/ConstantLattice.cpp
+++ llvm/src/lib/CSE231/ConstantLattice.cpp
@@ -51,8 +51,9 @@ void ConstantLattice::dump() {
     }
     else {
         for (map<string, ConstantInt *>::iterator i = _facts.begin(); i != _facts.end(); i++) {
-            errs() << "\t\t\tKey " << i->first << "\n";
-            errs() << "\t\t\tVal " << i->second->getSExtValue() << "\n";
+            errs() << "\t\t\t\t" << i->first << " -> " << i->second->getSExtValue() << "\n";
+            //errs() << "\t\t\tKey " << i->first << "\n";
+            //errs() << "\t\t\tVal " << i->second->getSExtValue() << "\n";
         }
     }
 }
